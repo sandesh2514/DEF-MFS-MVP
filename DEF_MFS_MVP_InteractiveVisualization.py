ce0b671de16805e86497f8d64c840efb6ede279a
rp-1221996 <54304418+rp-1221996@users.noreply.github.com>
2022-03-01 22:57:10 -0500

    Commit for visualizing data as dash-board

--- DEF_MFS_MVP_InteractiveVisualization.py
+++ DEF_MFS_MVP_InteractiveVisualization.py
@@ -2,14 +2,16 @@ try:
     # pip install --upgrade google-api-python-client
     # pip install --upgrade google-cloud-storage
     from google.cloud import storage
-    import google.cloud.storage
-    import json
     import os
     import sys
     import glob
     import pandas as pd
     import io
     from io import BytesIO
+    import dash
+    from dash import html
+    from dash import dcc
+    import plotly.express as px
 except Exception as e:
     print("Error : {} ".format(e))
 
@@ -19,7 +21,10 @@ storage_client = storage.Client.from_service_account_json(
 bucket = storage_client.get_bucket('bucket_stock')
 df_list = []
 
+app = dash.Dash(__name__)
+
 class IntVisual:
+
 
     def read_data(self):
         # Getting all files from GCP bucket
@@ -36,8 +41,27 @@ class IntVisual:
                     index_col=None
                 ))
 
-store = IntVisual()
-store.read_data()
+    def dash_board(self):
+        concatenated_df = pd.concat(df_list, ignore_index=True)
+        fig =px.line(concatenated_df, x="Date", y="Volume", title="Stock of Tesla")
+        app.title="Stock Prices"
 
-concatenated_df = pd.concat(df_list, ignore_index=True)
-print(concatenated_df)
+        app.layout = html.Div(
+            id="app-container",
+            children=[
+                html.H1("Stock of Tesla"),
+                html.P("Results in USD/oz"),
+                dcc.Graph(figure=fig)
+            ]
+        )
+
+Visual = IntVisual()
+Visual.read_data()
+Visual.dash_board()
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
+
+# concatenated_df = pd.concat(df_list, ignore_index=True)
+# print(concatenated_df)
+
