7ca7aaa54d6130af30ee896c7bd0e10e101c1ce4
rp-1221996 <54304418+rp-1221996@users.noreply.github.com>
2022-03-07 23:57:49 -0500

    Commit for visualizing data as dash-board

--- DEF_MFS_MVP_InteractiveVisualization.py
+++ DEF_MFS_MVP_InteractiveVisualization.py
@@ -1,3 +1,5 @@
+
+
 try:
     # pip install --upgrade google-api-python-client
     # pip install --upgrade google-cloud-storage
@@ -11,20 +13,27 @@ try:
     import dash
     from dash import html
     from dash import dcc
+    import dash_bootstrap_components as dbc
     import plotly.express as px
+    from dash.dependencies import Input, Output
+    import base64
 except Exception as e:
     print("Error : {} ".format(e))
 
 storage_client = storage.Client.from_service_account_json(
-            'C:\\Users\\Raj\\PycharmProjects\\Sensitive_Info\\DEF-MFS-MVP-Configuration.json')
+    'C:\\Users\\Raj\\PycharmProjects\\Sensitive_Info\\DEF-MFS-MVP-Configuration.json')
 
 bucket = storage_client.get_bucket('bucket_stock')
+
 df_list = []
 
-app = dash.Dash(__name__)
+external_stylesheets = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP, external_stylesheets])
+
+image_filename = 'stock_logo.jpg' # replace with your own image
+encoded_image = base64.b64encode(open(image_filename, 'rb').read())
 
 class IntVisual:
-
 
     def read_data(self):
         # Getting all files from GCP bucket
@@ -34,42 +43,141 @@ class IntVisual:
         for file in filename:
             df_list.append(pd.read_csv(
                 io.BytesIO(
-                    bucket.blob(blob_name = file).download_as_string()
-                    ),
-                    encoding = 'UTF-8',
-                    sep = ',',
-                    index_col=None
-                ))
+                    bucket.blob(blob_name=file).download_as_string()
+                ),
+                encoding='UTF-8',
+                sep=',',
+                index_col=None
+            ))
 
     def dash_board(self):
-        concatenated_df = pd.concat(df_list, ignore_index=True)
-        fig =px.line(concatenated_df,
-                     x="Date",
-                     y=["Volume"],
-                     title="Stock Prices between Jan-01-2021 to Dec-31-2021",
-                     color_discrete_map={"Tesla":"red"}
-                     )
 
-        fig.update_layout(
-            template="plotly_dark",
-            # xaxis_title="Date",
-            # yaxix_title="Price",
-            font=dict(
-                family="Verdana, sans-serif",
-                size=18,
-                color="white"
-            )
+        # styling the sidebar
+        SIDEBAR_STYLE = {
+            "position": "fixed",
+            "top": 0,
+            "left": 0,
+            "bottom": 0,
+            "width": "16rem",
+            "padding": "2rem 1rem",
+            "background-color": "#f8f9fa",
+            "text-align":"center",
+            "box-shadow": "1px 5px 10px #888888",
+        }
+
+        # padding for the page content
+        CONTENT_STYLE = {
+            "margin-left": "18rem",
+            "margin-right": "2rem",
+            "padding": "2rem 1rem",
+            "color": "grey"
+        }
+
+        # NAVBAR_STYLE={
+        #     "box-shadow": "5px 10px 8px #888888",
+        #     "width": 1600,
+        #     "left": 0,
+        #     "position": "fixed",
+        #     "margin - top": "-20px",
+        #     "height": "40px",
+        #     "background - color":  "# 668284",
+        # }
+
+        concatenated_df = pd.concat(df_list, ignore_index=True)
+
+        # navbar = dbc.NavbarSimple(
+        #     children=[
+        #         html.A(
+        #             # Use row and col to control vertical alignment of logo / brand
+        #             dbc.Row(
+        #                 [
+        #                     dbc.Col(html.Img(src='data:image/jpg;base64,{}'.format(encoded_image.decode()), height="60px")),
+        #                     dbc.Col(dbc.NavbarBrand("Navbar", className="ms-2")),
+        #                 ],
+        #
+        #             ),
+        #             style=NAVBAR_STYLE,
+        #         ),
+        #     ],
+        #     brand_href="#",
+        #     color="Light",
+        # )
+
+        sidebar = html.Div(
+            [
+                html.A(
+                    # Use row and col to control vertical alignment of logo / brand
+                    dbc.Row(
+                        [
+                            dbc.Col(
+                                html.Img(src='data:image/jpg;base64,{}'.format(encoded_image.decode()), height="120px"))
+                        ],
+
+                    )
+                ),
+                html.Hr(),
+                dbc.Nav(
+                    [
+                        dbc.NavLink("Dashboard", href="/", active="exact", className="fa fa-dashboard"),
+                        dbc.NavLink("Analytics", href="/analytics", active="exact", className="fa fa-line-chart"),
+                        dbc.NavLink("Comparison", href="/comparison", active="exact", className="fa fa-exchange"),
+                    ],
+                    vertical=True,
+                    pills=True,
+                ),
+            ],
+            style=SIDEBAR_STYLE,
         )
 
-        app.layout = html.Div(
-            id="app-container",
-            children=[
-                html.H1("Stocks of Four Companies"),
-                html.P("Results in USD/oz"),
-                dcc.Graph(figure=fig),
-                # html.title="Stock Prices between Jan-01-2021 to Dec-31-2021"
+        content = html.Div(id="page-content", children=[], style=CONTENT_STYLE)
+
+        app.layout = html.Div([
+            dcc.Location(id="url"),
+            # navbar,
+            sidebar,
+            content
+        ])
+
+    @app.callback(
+        Output("page-content", "children"),
+        [Input("url", "pathname")]
+    )
+    def render_page_content(pathname):
+        concatenated_df = pd.concat(df_list, ignore_index=True)
+
+        if pathname == "/":
+            return [
+                html.H4('Dashboard',
+                        style={'textAlign': 'left'}),
+                dcc.Graph(id='bargraph',
+                          figure=px.bar(concatenated_df, barmode='group', x='Date',
+                                        y=['Volume']))
+            ]
+        elif pathname == "/analytics":
+            return [
+                html.H4('Analytics',
+                        style={'textAlign': 'left'}),
+                dcc.Graph(id='bargraph',
+                          figure=px.bar(concatenated_df, barmode='group', x='Date',
+                                        y=['Volume']))
+            ]
+        elif pathname == "/comparison":
+            return [
+                html.H4('Comparison',
+                        style={'textAlign': 'left'}),
+                dcc.Graph(id='bargraph',
+                          figure=px.bar(concatenated_df, barmode='group', x='Date',
+                                        y=['Volume']))
+            ]
+        # If the user tries to reach a different page, return a 404 message
+        return dbc.Jumbotron(
+            [
+                html.H1("404: Not found", className="text-danger"),
+                html.Hr(),
+                html.P(f"The pathname {pathname} was not recognised..."),
             ]
         )
+
 
 Visual = IntVisual()
 Visual.read_data()
@@ -77,7 +185,3 @@ Visual.dash_board()
 
 if __name__ == "__main__":
     app.run_server(debug=True)
-
-# concatenated_df = pd.concat(df_list, ignore_index=True)
-# print(concatenated_df)
-
